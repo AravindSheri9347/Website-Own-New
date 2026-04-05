892af88efb331dbb7f2f46fd43e343a3e03f9b4b
AravindSheri9347 <aravindsheri88@gmail.com>
2026-04-05 14:48:49 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -5,9 +5,9 @@ import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.0/firebas
 
 // Your config (copy from Firebase console)
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT.firebaseapp.com",
-  projectId: "YOUR_PROJECT_ID",
+  apiKey: "AIzaSyB5RPmnslaA3mXVIPxapx7YMfLSp6wlA6A",
+  authDomain: "chitchat-1a684.firebaseapp.com",
+  projectId: "chitchat-1a684",
 };
 
 // Initialize
