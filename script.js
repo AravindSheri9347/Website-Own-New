8a3cbfec702f2b26e10108f109a01ec18d9e2a4e
AravindSheri9347 <aravindsheri88@gmail.com>
2026-04-07 15:03:37 +0530

    Update script.js

--- script.js
+++ script.js
@@ -31,8 +31,8 @@ const firebaseConfig = {
   apiKey: "AIzaSyB5RPmnslaA3mXVIPxapx7YMfLSp6wlA6A",
   authDomain: "chitchat-1a684.firebaseapp.com",
   projectId:  "chitchat-1a684",
-  // storageBucket: "chitchat-1a684.firebasestorage.app",
-  storageBucket: "chitchat-1a684.appspot.com",
+  storageBucket: "chitchat-1a684.firebasestorage.app",
+  // storageBucket: "chitchat-1a684.appspot.com",
   messagingSenderId:  "753237671940",
   appId: "1:753237671940:web:c1bfc1b2c21aee16dc3551",
 };
