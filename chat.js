847a16a303becfa9eee4fef15c0b9961495f32b1
AravindSheri9347 <aravindsheri88@gmail.com>
2026-04-08 20:51:09 +0530

    Create chat.js

---
+++ chat.js
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Chat</title>
+  <link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+<div class="chat-container">
+
+  <div id="messages"></div>
+
+  <input id="msg" placeholder="Type message">
+  <button onclick="sendMsg()">Send</button>
+
+</div>
+
+<script type="module" src="chat.js"></script>
+
+</body>
+</html>
