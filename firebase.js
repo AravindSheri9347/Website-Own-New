e19aefbe5f2003d0e8c24ae32825ff4e8d7188de
AravindSheri9347 <aravindsheri88@gmail.com>
2026-04-08 20:48:06 +0530

    firebase credentials

--- firebase.js
+++ firebase.js
@@ -1,15 +1,13 @@
-// Firebase imports
-// import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
-// import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
-// import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
 
-// // Your config (copy from Firebase console)
-// const firebaseConfig = {
-//   
+const firebaseConfig = {
+  apiKey: "AIzaSyB5RPmnslaA3mXVIPxapx7YMfLSp6wlA6A",
+  authDomain: "chitchat-1a684.firebaseapp.com",
+  projectId:  "chitchat-1a684",
+  storageBucket: "chitchat-1a684.firebasestorage.app",
+  // storageBucket: "chitchat-1a684.appspot.com",
+  messagingSenderId:  "753237671940",
+  appId: "1:753237671940:web:c1bfc1b2c21aee16dc3551",
+};
 
-// // Initialize
-// const app = initializeApp(firebaseConfig);
-
-// // Export services
-// export const auth = getAuth(app);
-// export const db = getFirestore(app);
+export const app = initializeApp(firebaseConfig);
