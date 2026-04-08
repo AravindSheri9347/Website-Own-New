3392e12340a699f148a86a610214eeab73225b6c
AravindSheri9347 <aravindsheri88@gmail.com>
2026-04-08 20:23:02 +0530

    Update script.js

--- script.js
+++ script.js
@@ -28,13 +28,7 @@ import {
 
 // FIREBASE CONFIG
 const firebaseConfig = {
-  apiKey: "AIzaSyB5RPmnslaA3mXVIPxapx7YMfLSp6wlA6A",
-  authDomain: "chitchat-1a684.firebaseapp.com",
-  projectId:  "chitchat-1a684",
-  storageBucket: "chitchat-1a684.firebasestorage.app",
-  // storageBucket: "chitchat-1a684.appspot.com",
-  messagingSenderId:  "753237671940",
-  appId: "1:753237671940:web:c1bfc1b2c21aee16dc3551",
+ 
 };
 
 const app = initializeApp(firebaseConfig);
