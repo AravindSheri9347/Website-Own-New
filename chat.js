860eaa86ebd4473b88a5af739a5aac8037869dc5
AravindSheri9347 <aravindsheri88@gmail.com>
2026-04-09 10:29:45 +0530

    old names,new codes

--- chat.js
+++ chat.js
@@ -1,21 +1,40 @@
-<!DOCTYPE html>
-<html>
-<head>
-  <title>Chat</title>
-  <link rel="stylesheet" href="style.css">
-</head>
-<body>
+import { app } from "./firebase.js";
 
-<div class="chat-container">
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
 
-  <div id="messages"></div>
+import { 
+  getFirestore, addDoc, collection, 
+  serverTimestamp, query, orderBy, onSnapshot 
+} from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
 
-  <input id="msg" placeholder="Type message">
-  <button onclick="sendMsg()">Send</button>
+const auth = getAuth(app);
+const db = getFirestore(app);
 
-</div>
+window.sendMsg = async function () {
+  const text = document.getElementById("msg").value;
 
-<script type="module" src="chat.js"></script>
+  await addDoc(collection(db, "messages"), {
+    text,
+    uid: auth.currentUser.uid,
+    time: serverTimestamp()
+  });
 
-</body>
-</html>
+  document.getElementById("msg").value = "";
+};
+
+const messagesDiv = document.getElementById("messages");
+
+const q = query(collection(db, "messages"), orderBy("time"));
+
+onSnapshot(q, (snapshot) => {
+  messagesDiv.innerHTML = "";
+
+  snapshot.forEach(doc => {
+    const data = doc.data();
+
+    const div = document.createElement("div");
+    div.innerText = data.uid + ": " + data.text;
+
+    messagesDiv.appendChild(div);
+  });
+});
